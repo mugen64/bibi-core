042a3030d81d3326f5d408eb36da2036d2959e5d
Kyosimba <faridah6931@live.com>
2026-07-29 00:04:52 +0300

    feat: transcribe wav file

--- stt/api/models.py
+++ stt/api/models.py
@@ -1,8 +1,11 @@
+import math
+
 from dataclasses import dataclass
 from typing import Any
 from enum import Enum
 
 from pydantic import BaseModel, Field
+from engines.models import Transcript,TranscriptSegment
 
 
 
@@ -16,7 +19,34 @@ class ApiResponse:
 class AudioFormat(str, Enum):
     WAV = "wav"
 
-@dataclass
-class TranscriptionResponse(BaseModel):
-    segments:list[dict]
+class SegmentResponse(BaseModel):
+    start_ms: int
+    end_ms: int
+    score: float | None
+    text: str
 
+
+class TranscriptionResponse(BaseModel):
+    segments:list[SegmentResponse]
+
+def transcript_to_response(transcript: Transcript)-> TranscriptionResponse:
+    return TranscriptionResponse(
+        segments=[
+            SegmentResponse(
+                start_ms=s.start_ms,
+                end_ms=s.end_ms,
+                text=s.text,
+                score=clean_nan(s.score)
+            )
+            for s in transcript.segments
+        ]
+    )
+
+def clean_nan(obj):
+    if isinstance(obj, float):
+        return None if math.isnan(obj) else obj
+    if isinstance(obj, dict):
+        return {k: clean_nan(v) for k, v in obj.items()}
+    if isinstance(obj, list):
+        return [clean_nan(v) for v in obj]
+    return obj
--- stt/api/routes.py
+++ stt/api/routes.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, HTTPException, WebSocket
+from fastapi import APIRouter, HTTPException, WebSocket,Form,File,UploadFile
 from fastapi.responses import StreamingResponse, Response
 
-from api.models import ApiResponse
+from api.models import ApiResponse,transcript_to_response
 from engine_manager import STTManager
+from config import DEFAULT_MODEL
+from engines.audio_sources import WavFileAudioSource
 
 router = APIRouter()
 sm = STTManager()
@@ -16,3 +18,31 @@ def list_voices():
         message=f"{len(models)} available models"
     )
 
+@router.post("/transcribe")
+async def transcribe(
+            file: UploadFile=File(...),
+            model: str=None,
+):
+    print(file.content_type)
+    if file.content_type != "audio/wav":
+        raise HTTPException(
+            status_code=400,
+            detail="Only wav files are supported",
+        )
+    if model is None:
+        model = DEFAULT_MODEL
+
+    temp_file = f"/tmp/{file.filename}"
+    with open(temp_file, "wb") as f:
+        f.write(await file.read())
+
+    src = WavFileAudioSource(temp_file)
+    engine = sm.get_engine(model)
+
+    script = engine.transcribe(src)
+
+    return ApiResponse(
+        status=200,
+        message="Transciption succeeded",
+        data=transcript_to_response(script)
+    )
